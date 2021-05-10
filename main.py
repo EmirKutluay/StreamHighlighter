068c00884de699f8f277c61ca31a44891fd54587
Emir Kutluay <42154741+EmirKutluay@users.noreply.github.com>
2021-05-10 18:17:37 +0300

    SH

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import librosa
 from pydub import AudioSegment
+from scipy.io import wavfile
+import moviepy.editor as mp
 
-test_file = r"C:\Users\memir\Desktop\testaudio.wav"
-folder_loc = r"C:\Users\memir\Desktop\testfolder\\"
+video_file = "fidyo.mp4"
+audio_file = "audio.wav"
+mic_file = "audio_mic.wav"
 folder_type = ".wav"
 
-test, sr = librosa.load(test_file)
+video = mp.VideoFileClip(video_file)
+video.audio.write_audiofile(audio_file)
 
-new = librosa.effects.split(test, top_db=45)
+fs, audio = wavfile.read(folder_loc)
+wavfile.write(mic_file, fs, audio[:, 1])
+
+test, sr = librosa.load(mic_file)
+
+selected_times = librosa.effects.split(test, top_db=25)
 
 sample_duration = 1 / sr
 sample_num_each_milisecond = 0.001/sample_duration
 
 i = 0
-for x in new:
+for x in selected_times:
     start = x[0] / sample_num_each_milisecond - 100
     end = x[1] / sample_num_each_milisecond + 100
-    audio = AudioSegment.from_wav(r"C:\Users\memir\Desktop\testaudio.wav")
+    audio = AudioSegment.from_file(mic_file)
     extract = audio[start:end]
-    export_loc = folder_loc + str(i) + folder_type
+    export_loc = str(i) + folder_type
     print(export_loc)
     extract.export(export_loc, format="wav")
     i += 1
